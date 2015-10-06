f6acda23a58529cbf9e41b33652f817e5c1d7bc3
Ian Palmgren <ianmpal@gmail.com>
2015-10-05 20:18:42 -0700

    Hello, iOS

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Ian Palmgren on 10/5/15.
+//  Copyright © 2015 Ian Palmgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
